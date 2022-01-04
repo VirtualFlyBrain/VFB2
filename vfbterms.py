dddf20c4e20831d3905bf3614fa1587c51069eb5
Rob Court <rcourt@ed.ac.uk>
2022-01-04 17:30:05 +0000

    typo fix

--- vfbterms.py
+++ vfbterms.py
@@ -142,4 +142,4 @@ save_terms(vc.nc.commit_list(["MATCH (n:Class) WHERE n.short_form starts with 'V
 save_terms(vc.nc.commit_list(["MATCH (n:pub) with n.short_form as id ORDER BY id ASC RETURN collect(distinct id) as ids"])[0]['data'][0]['row'][0])
 chdir(mypath + '../datasets/')
 
-save_terms(vc.nc.commit_list(["MATCH (n:Dataset) with n.short_form as id ORDER BY id ASC RETURN collect(distinct id) as ids"])[0]['data'][0]['row'][0])
+save_terms(vc.nc.commit_list(["MATCH (n:DataSet) with n.short_form as id ORDER BY id ASC RETURN collect(distinct id) as ids"])[0]['data'][0]['row'][0])
