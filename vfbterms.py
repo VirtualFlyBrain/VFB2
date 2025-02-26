f2e3ff3c59325077854c40a8fb2cbede663d55cb
Rob Court <robbie.court@gmail.com>
2025-02-26 10:52:51 +0000

    Enhance term documentation generation closer to text version of VFB site term info

--- vfbterms.py
+++ vfbterms.py
@@ -2,6 +2,7 @@ import sys
 from os import listdir, chdir
 from os.path import isfile, join
 from vfb_connect.cross_server_tools import VfbConnect
+import re
 
 version = 6
 
@@ -108,6 +109,80 @@ def save_terms(ids):
         except:
             print("ERROR:" + id)
 
+def get_term_url(label, short_form):
+    """Create canonical URL for a term"""
+    url = label.replace('\\','').replace(' ','-').lower() + "-" + short_form.lower()
+    return re.sub("[^0-9a-zA-Z-_]+", "", url)
+
+def format_relationships(term):
+    rels = []
+    if "relationships" in term:
+        for rel in term["relationships"]:
+            obj = rel["object"]
+            types = ""
+            if "types" in obj:
+                types = " ".join([f'<span class="label label-{t}">{t}</span>' for t in obj["types"]])
+            term_url = get_term_url(obj["label"], obj["short_form"])
+            rels.append(f'- {rel["relation"]["label"]} [{obj["label"]}](https://www.virtualflybrain.org/term/{term_url}) <span class="label types">{types}</span>')
+    return "\n".join(rels)
+
+def format_classification(term):
+    classes = []
+    if "parents" in term:
+        for parent in term["parents"]:
+            types = ""
+            if "types" in parent:
+                types = " ".join([f'<span class="label label-{t}">{t}</span>' for t in parent["types"]])
+            term_url = get_term_url(parent["label"], parent["short_form"])
+            classes.append(f'- [{parent["label"]}](https://www.virtualflybrain.org/term/{term_url}) <span class="label types">{types}</span>')
+    return "\n".join(classes)
+
+def format_synonyms(term):
+    syns = []
+    if "pub_syn" in term:
+        for syn in term["pub_syn"]:
+            pub_link = ""
+            if "pub" in syn and "core" in syn["pub"]:
+                pub = syn["pub"]["core"]
+                term_url = get_term_url(pub["label"], pub["short_form"])
+                pub_link = f' ([{pub["label"]}](https://www.virtualflybrain.org/term/{term_url}))'
+            syns.append(f'- {syn["synonym"]["scope"]}: {syn["synonym"]["label"]}{pub_link}')
+    return "\n".join(syns)
+
+def format_xrefs(term):
+    refs = []
+    if "xrefs" in term:
+        for xref in term["xrefs"]:
+            icon = ""
+            if "icon" in xref:
+                icon = f'<img class="terminfo-siteicon" src="{xref["icon"]}">'
+            refs.append(f'- <a href="{xref["link_base"]}{xref["accession"]}" target="_blank">{xref["site"]["label"]}</a>{icon}')
+            if "link_text" in xref:
+                refs.append(f'  - <a href="{xref["link_base"]}{xref["accession"]}" target="_blank">{xref["link_text"]}</a>')
+    return "\n".join(refs)
+
+def format_references(term):
+    refs = []
+    if "def_pubs" in term:
+        for pub in term["def_pubs"]:
+            cite = [f'- <a href="?id={pub["core"]["short_form"]}">{pub["core"]["label"]}</a>']
+            xrefs = []
+            if "FlyBase" in pub:
+                xrefs.append(f'<a href="http://flybase.org/reports/{pub["FlyBase"]}" target="_blank">'
+                           f'<i class="popup-icon-link gpt-fly" title="FlyBase:{pub["FlyBase"]}"></i></a>')
+            if "DOI" in pub:
+                xrefs.append(f'<a href="https://doi.org/{pub["DOI"]}" target="_blank">'
+                           f'<i class="popup-icon-link gpt-doi" title="doi:{pub["DOI"]}"></i></a>')
+            if "PubMed" in pub:
+                xrefs.append(f'<a href="http://www.ncbi.nlm.nih.gov/pubmed/?term={pub["PubMed"]}" target="_blank">'
+                           f'<i class="popup-icon-link gpt-pubmed" title="PMID:{pub["PubMed"]}"></i></a>')
+            if xrefs:
+                cite.append(f'<span class="terminfo-pubxref"> {" ".join(xrefs)}</span>')
+            if "types" in pub["core"]:
+                types = " ".join([f'<span class="label label-{t}">{t}</span>' for t in pub["core"]["types"]])
+                cite.append(f'<span class="label types">{types}</span>')
+            refs.append(" ".join(cite))
+    return "\n".join(refs)
 
 def wrapStringInHTMLMac(term):
     import datetime
@@ -120,7 +195,9 @@ def wrapStringInHTMLMac(term):
     if not os.path.isfile(filename):
         f = open(filename, "w", encoding="utf-8")
         images = ""
+        images_list = []
         images = " ".join(find_images(term, "image_folder", set()))
+        images_list = ",".join(find_images_list(term, "image_folder", set()))
         if "<img" in images:
             images = '## Example Images\n' + images
         desc = ""
@@ -133,7 +210,6 @@ def wrapStringInHTMLMac(term):
             com = ' '.join(term["term"]["comment"]).replace("\n", " ").replace('\r', ' ')
         except:
             print('missing desc')
-        # If no description then combine all 'label' in the json to give a a crude description of term, xrefs, technique & examples.
         try:
             if desc == "":
                 com += " [" + "; ".join(find_values(term, "label", set())) + "]"
@@ -141,7 +217,6 @@ def wrapStringInHTMLMac(term):
             print('error on label for desc')
             print(e)
             print(traceback.format_exc())
-        # Add Ontology to tags list (XXX from XXX_YYYYYYYY)
         try:
             tags = ','.join(term["term"]["core"]["types"])
             if "_" in term["term"]["core"]["short_form"]:
@@ -152,17 +227,38 @@ def wrapStringInHTMLMac(term):
             print('error on tag creation')
             print(e)
             print(traceback.format_exc())
-        # Add extra meta 
-        try:
-            synonyms = ", ".join([syn["val"] for syn in term["term"].get("synonyms", [])])
-            term_type = term["term"].get("type", "")
-        except Exception as e:
-            print('error on extra meta creation')
-            print(e)
-            print(traceback.format_exc())
+        
+        classification = format_classification(term)
+        relationships = format_relationships(term)
+        synonyms = format_synonyms(term)
+        xrefs = format_xrefs(term)
+        references = format_references(term)
+        
         url = term["term"]["core"]["label"].replace('\\','&bsol;').replace(' ','-').lower()+"-"+term["term"]["core"]["short_form"].lower()
         url = re.sub("[^0-9a-zA-Z-_]+", "", url)
-        whole = wrapper.format(term["term"]["core"]["label"].replace('\\','&bsol;'),term["term"]["core"]["short_form"],desc,com,tags,json.dumps(term, indent=4),images,now,note,url,synonyms,term_type)
+        
+        term_type = ','.join(term["term"]["core"].get("types", []))
+        
+        whole = wrapper.format(
+            term["term"]["core"]["label"].replace('\\','&bsol;'),  # {0}
+            term["term"]["core"]["short_form"],                    # {1}
+            desc,                                                  # {2}
+            com,                                                   # {3}
+            tags,                                                  # {4}
+            json.dumps(term, indent=4),                           # {5}
+            images,                                               # {6}
+            now,                                                  # {7}
+            images_list,                                         # {8}
+            url,                                                 # {9}
+            note,                                                # {10}
+            synonyms,                                            # {11}
+            term_type,                                          # {12}
+            classification,                                      # {13}
+            relationships,                                       # {14}
+            xrefs,                                              # {15}
+            references                                          # {16}
+        )
+        
         try:
             f.write(whole)
             filename = term["term"]["core"]["short_form"] + "_v" + str(version - 1) + ".md"
@@ -173,9 +269,7 @@ def wrapStringInHTMLMac(term):
             print(whole)
         f.close()
 
-
 vc=VfbConnect(neo_endpoint='http://pdb.virtualflybrain.org', neo_credentials=('neo4j', 'vfb'), owlery_endpoint='http://owl.virtualflybrain.org/kbs/vfb/')
-
 
 mypath = sys.argv[1]
 print("Updating all files in " + mypath)
